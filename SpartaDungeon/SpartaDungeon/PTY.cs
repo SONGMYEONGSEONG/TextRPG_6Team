59bb6e49cad240cf33e6c6719722dd1bf16b6ec6
Parktaeyoung <xoddlvhvh@naver.com>
2024-09-27 14:27:40 +0900

    Add pty
    test

---
+++ SpartaDungeon/SpartaDungeon/PTY.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeon
+{
+    internal class PTY
+    {
+        int a = 10;
+        
+    }
+}
