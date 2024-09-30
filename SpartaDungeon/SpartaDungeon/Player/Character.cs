5afd502bf94dec14dbab6d41fd11cf2d5510b280
Sungchan Ahn <146975762+sungchanahn@users.noreply.github.com>
2024-09-30 10:44:37 +0900

    [Add] Character Property & update character stat method

--- SpartaDungeon/SpartaDungeon/Player/Character.cs
+++ SpartaDungeon/SpartaDungeon/Player/Character.cs
@@ -21,18 +21,41 @@ namespace SpartaDungeon
         public int Level { get; set; }
         public JobType CharacterJobType { get; set; }
         public string Job { get; set; }
+
         public float MaxHp { get; set; }
-        public float CurrentHp { get; set; }
+        public float ExtraHp { get; set; }
+        public float TotalMaxHp { get; set; }
+        public float? CurrentHp { get; set; }
+
         public float MaxMp { get; set; }
-        public float CurrentMp { get; set; }
+        public float ExtraMp { get; set; }
+        public float TotalMaxMp { get; set; }
+        public float? CurrentMp { get; set; }
+
         public float Attack { get; set; }       // Atk          : 기본 공격 능력치
         public float ExtraAttack { get; set; }  // ExtraAtk     : 장착된 장비 아이템 추가 수치
-        public float Defense { get; set; }      // Def          : 
+        public float TotalAttack { get; set; }
+
+
+        public float Defense { get; set; }      // Def
         public float ExtraDefense { get; set; } // ExtraDef
+        public float TotalDefense { get; set; }
+
+
         public float Agility { get; set; }      // Agl          : 민첩. 내 회피 확률에 영향
+        // public float ExtraAgility { get; set; }
+        // public float TotalAgility { get; set; }
+
         public float Accuracy { get; set; }     // Acc          : 명중. 상대의 회피 확률과 내 공격 크리티컬 확률에 영향
+        // public float ExtraAccuracy { get; set; }
+        // public float TotalAccuracy { get; set; }
+
         public float Luck { get; set; }         // Luc of Luk   : 행운. 공격 크리티컬 확률, 보상에 영향
+
         public float? Intelligence { get; set; }
+        // public float ExtraIntelligence { get; set; }
+        // public float TotalIntelligence { get; set; }
+
         public int Gold { get; set; }
 
         public List<Item> Inventory = new List<Item>();
@@ -57,23 +80,54 @@ namespace SpartaDungeon
 
         public Character(string _name, int _jobTypeNum)
         {
+            // 디폴트 값
             Name = _name;
             Level = 1;
+            MaxHp = 100f;
+            MaxMp = 100f;
+            Attack = 10f;
+            Defense = 10f;
+            Agility = 10f;
+            Accuracy = 10f;
+            Luck = 10f;
+            Intelligence = null;
+            Gold = 500;
+
             if (_jobTypeNum == (int)JobType.Warrior)
             {
-                // 초기스탯 세팅
+                Job = "전사";
+                MaxHp = 150f;
+                MaxMp = 50f;
+                Attack = 15f;
+                Defense = 15f;
             }
             else if (_jobTypeNum == (int)JobType.Rogue)
             {
-
+                Job = "도적";
+                Attack = 13f;
+                Agility = 15f;
+                Luck = 20f;
             }
             else if (_jobTypeNum == (int)JobType.Archer)
             {
-
+                Job = "궁수";
+                MaxHp = 80f;
+                MaxMp = 120f;
+                Attack = 15f;
+                Defense = 8f;
+                Agility = 20f;
+                Accuracy = 15f;
             }
             else if (_jobTypeNum == (int)JobType.Mage)
             {
-                // 초기스탯 세팅
+                Job = "마법사";
+                MaxHp = 70f;
+                MaxMp = 200f;
+                Attack = 5f;
+                Defense = 5f;
+                Agility = 5f;
+                Accuracy = 5f;
+                Luck = 15f;
                 Intelligence = 20f;
             }
         }
@@ -97,20 +151,24 @@ namespace SpartaDungeon
         }
 
         // 플레이어 스탯 갱신
-        public void SetStat()
+        public void UpdateStat()
         {
-
-        }
-
-        // 플레이어 상태 보기
-        public void DisplayStatus()
-        {
-
-            // 플레이어 스탯
-            if (Intelligence != null)
+            if (CurrentHp == null)
             {
-                Console.WriteLine($"지능: {Intelligence}");
+                CurrentHp = TotalMaxHp;
             }
+            if (CurrentMp == null)
+            {
+                CurrentMp = TotalMaxMp;
+            }
+            ExtraHp = EquipWeapon.AdditionalHP + EquipArmor.AdditionalHP; //
+            // ExtraMp = EquipWeapon.AdditionalMP + EquipArmor.AdditionalMP;
+            ExtraAttack = EquipWeapon.Atk + EquipArmor.Atk; // + EquipSubWeapon.Atk
+            ExtraDefense = EquipWeapon.Def + EquipArmor.Def; // + wearSubWeapon.Def
+            TotalMaxHp = MaxHp + ExtraHp;
+            TotalMaxMp = MaxMp + ExtraMp;
+            TotalAttack = Attack + ExtraAttack;
+            TotalDefense = Defense + ExtraDefense;
         }
     }
 }
