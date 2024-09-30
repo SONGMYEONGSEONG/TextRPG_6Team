bd88f5b7d7e002d3736fc4b848ab358a579fa979
Sungchan Ahn <146975762+sungchanahn@users.noreply.github.com>
2024-09-30 19:36:09 +0900

    [Fix] 기존 GameManage 삭제 후 introScene의 RunGame 호출

---
+++ SpartaDungeon/SpartaDungeon/RunRPG.cs
@@ -0,0 +1,11 @@
+﻿namespace SpartaDungeon
+{
+    internal class RunRPG
+    {
+        static void Main(string[] args)
+        {
+            IntroScene introScene = new IntroScene();
+            introScene.RunGame();
+        }
+    }
+}
