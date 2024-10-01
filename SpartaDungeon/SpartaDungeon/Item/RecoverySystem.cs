e393b983874711ed1c789e638c6ed406de94b31d
hi3816 <wlgp2534@naver.com>
2024-10-02 02:56:55 +0900

    UPD Character.cs ...
    item.json - 아이템 정보 수정
    RecoverySystem - 회복 클래스 수정
    Store.cs - 수정 중
    Character.cs - ManageRecovery

--- SpartaDungeon/SpartaDungeon/Item/RecoverySystem.cs
+++ SpartaDungeon/SpartaDungeon/Item/RecoverySystem.cs
@@ -8,36 +8,61 @@ namespace SpartaDungeon
 {
     internal class RecoverySystem
     {
-        public float? CurrentHP { get; private set; }
-        public float MaxHP { get; private set; }
+        public float? CurrentHp { get; private set; }
+        public float MaxHp { get; private set; }
+        public float? CurrentMp { get; private set; }
+        public float MaxMp { get; private set; }
 
         // 회복량을 저장하는 변수
-        private int recoveryAmount;
+        private float recoveryAmount;
 
-        // 생성자: 초기 체력과 최대 체력을 설정
-        public RecoverySystem(float maxHP, float? currentHP)
+        // 체력 초기화
+        public void InitializeHp(float maxHp, float? currentHp)
         {
-            MaxHP = maxHP;
-            CurrentHP = currentHP;
+            MaxHp = maxHp;
+            CurrentHp = currentHp;
         }
 
-        // 회복 기능
+        // 마나 초기화
+        public void InitializeMp(float maxMp, float? currentMp)
+        {
+            MaxMp = maxMp;
+            CurrentMp = currentMp;
+        }
+
+        // 체력 회복 기능
         public float? HpRecover(string itemNum)
         {
-            //포션종류에 따른 회복량
-            if (itemNum == "007") { recoveryAmount = 100; }
-            else if (itemNum == "008") { recoveryAmount = 80; }
+            // 포션 종류에 따른 회복량
+            if (itemNum == "501") { recoveryAmount = 100f; }
 
             // 현재 체력에 회복량 추가, 최대 체력을 넘지 않도록 조정
-            CurrentHP += recoveryAmount;
-            if (CurrentHP > MaxHP)
+            CurrentHp += recoveryAmount;
+            if (CurrentHp > MaxHp)
             {
-                CurrentHP = MaxHP;
+                CurrentHp = MaxHp;
             }
-            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
-            Console.WriteLine($"플레이어가 {recoveryAmount}만큼 회복했습니다.");
+            Console.WriteLine("--------------");
+            Console.WriteLine(CurrentHp);
+            Console.WriteLine("--------------");
 
-            return CurrentHP;  
+            return CurrentHp;
+        }
+
+        // 마나 회복 기능 추가
+        public float? MpRecover(string itemNum)
+        {
+            // 포션 종류에 따른 회복량 (예시)
+            if (itemNum == "005") { recoveryAmount = 50f; }
+
+            // 현재 마나에 회복량 추가, 최대 마나를 넘지 않도록 조정
+            CurrentMp += recoveryAmount;
+            if (CurrentMp > MaxMp)
+            {
+                CurrentMp = MaxMp;
+            }
+
+            return CurrentMp;
         }
     }
 }
